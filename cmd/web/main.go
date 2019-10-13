5daee120bb4de0580e1bcaf416c6126ff38b770c
Jason Deutsch <jndeutsch@gmail.com>
2019-10-13 18:51:20 -0500

    fix init postgres in main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -106,8 +106,8 @@ func initMongo() (*mongo.Database, error) {
 }
 
 func initPostgres() (*sql.DB, error) {
-	connectionInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	connectionInfo := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_DB"))
 
 	db, err := sql.Open("postgres",connectionInfo )
 	return db, err
